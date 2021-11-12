edbb9040dd82bd3b9ba01fd29d479a9158f8873a
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-11-12 09:48:22 +0200

    Initial setup

---
+++ NameSpaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NameSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
